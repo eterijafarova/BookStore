1475b33a27ca02899939de62defecd5b9ca5dea7
Eteri Jafarova <etericeferova2005@gmail.com>
2025-02-27 22:45:11 +0400

    first commit

---
+++ BookShop/Data/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace BookShop.Data.Models;
+
+public class Category
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Name { get; set; } 
+    
+    public ICollection<Book> Books { get; set; } = new List<Book>();
+}
---
+++ BookShop/Data/Models/Order.cs
@@ -0,0 +1,13 @@
+﻿namespace BookShop.Data.Models;
+
+public class Order
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public Guid UserId { get; set; }
+    public decimal TotalPrice { get; set; }
+    public string Status { get; set; } = ""; //нужно написать статусы сюда, думаю
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public User User { get; set; } = null!;
+    public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+}
